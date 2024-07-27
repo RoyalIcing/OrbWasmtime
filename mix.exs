6cace2b178c7edc26d2285fb9ca326f2078d45fd
Patrick Smith <pgwsmith@gmail.com>
2024-07-27 20:53:47 +1000

    Bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule OrbWasmtime.MixProject do
   use Mix.Project
 
-  @version "0.1.16"
+  @version "0.1.17"
   @source_url "https://github.com/RoyalIcing/orb_wasmtime"
   @force_build? System.get_env("ORB_WASMTIME_BUILD") in ["1", "true"]
 
